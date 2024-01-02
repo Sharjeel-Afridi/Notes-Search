3969a8746690682f6b3d0009860d38088f08be29
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2024-01-02 15:36:52 +0530

    full working

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const inputfield = document.getElementById('input-el');
+const submit = document.getElementById('submit');
+const apiURL =  'https://notes-search.pockethost.io/api/collections/notes/records';
+const newDiv = document.createElement('div');
+newDiv.setAttribute('class','new-div');
+const mainDiv = document.querySelector('.main');
+
+
+const fuseOptions = {
+    keys: ['Subjects'],
+    threshold: 0.5
+};
+
+const fuse = new Fuse([], fuseOptions);
+
+async function notesSearch(searchTerm){
+    const loadingDiv = document.createElement('div');
+    loadingDiv.innerHTML = `<div class="lds-ellipsis"><div></div><div></div><div></div><div>`;
+    mainDiv.appendChild(loadingDiv);
+    const newTerm = searchTerm.toLowerCase();
+    const apiResponse = await fetch(apiURL).then((resp)=>{return resp.json()}).then(data => {return data});
+    // console.log(apiResponse)
+    
+    fuse.setCollection(apiResponse.items);
+
+    
+    const results = fuse.search(newTerm);
+    // console.log(results);
+    // const matchingAnswers = results.map(el => el.item.notes);
+    // console.log(`Search results for "${searchTerm}":`,matchingAnswers);
+
+    results.forEach(element => {
+        const notesDiv = document.createElement('div');
+        notesDiv.classList.add('notes-div');
+        notesDiv.setAttribute('onclick',`redirect('${element.item.notes}')`);
+        notesDiv.innerHTML = `<h3>${element.item.Title}</h3>`;
+        newDiv.appendChild(notesDiv);
+    });
+    loadingDiv.innerHTML = '';
+    mainDiv.removeChild(loadingDiv);
+    
+}
+submit.addEventListener('click', () =>{
+    const searchTerm = inputfield.value;
+    newDiv.innerHTML = '';
+    notesSearch(searchTerm)
+    document.body.appendChild(newDiv)
+    console.log('button')
+})
+
+function redirect(url) {
+    window.open(url, '_blank');
+}
